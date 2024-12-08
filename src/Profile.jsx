c74351cd92a6d317cce258e58702da6c3a187bd3
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-12-08 18:34:10 +0300

    s

---
+++ src/Profile.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div>
+      <div>
+        <img
+          src={image || 'https://via.placeholder.com/150'}
+          alt="User avatar"
+        />
+        <p>{name || 'Unknown User'}</p>
+        <p>@{tag || 'unknown'}</p>
+        <p>{location || 'Unknown Location'}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers: </span>
+          <span>{stats?.followers || 0}</span>
+        </li>
+        <li>
+          <span>Views: </span>
+          <span>{stats?.views || 0}</span>
+        </li>
+        <li>
+          <span>Likes: </span>
+          <span>{stats?.likes || 0}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
