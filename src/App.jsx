7ef3dbc690b5eae6d597bb4b16b8433febeb0b1c
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-12-09 21:49:31 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Profile from './Profile/Profile';
-import FriendList from './FriendList/FriendList';
 import userData from './Profile/userData.json';
+
+import FriendList from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
 
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import transactions from "./TransactionHistory/transactions.json";
+
+const toCapitalCase = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
 const App = () => {
+  
+  const modifiedTransactions = transactions.map((transaction) => ({
+    ...transaction,
+    type: toCapitalCase(transaction.type),
+  }));
+
   return (
     <div>
       <Profile
@@ -12,10 +24,10 @@ const App = () => {
         tag={userData.tag}
         location={userData.location}
         image={userData.avatar}
-        stats={userData.stats}
-      />
-      <h2>My Friends</h2>
+        stats={userData.stats} />
+      
       <FriendList friends={friends} />
+      <TransactionHistory items={modifiedTransactions} />
     </div>
   );
 };
